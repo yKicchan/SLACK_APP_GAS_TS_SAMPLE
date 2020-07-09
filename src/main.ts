7cbeaddf47e74aaa7a6f71df2711dbe1a8f90c02
yKicchan <ykicchanapp@gmail.com>
2020-07-10 00:15:54 +0900

    feat: dayjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import dayjs from "dayjs";
+import "dayjs/locale/ja";
+
+dayjs.locale("ja");
+
 export function doPost() {
+  const text = dayjs().format("YYYY年M月D日(ddd) H:m");
+
   const data: SlackData = {
-    text: "Hello world!!",
+    text,
     response_type: "ephemeral",
   };
+
   return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(
     ContentService.MimeType.JSON
   );
